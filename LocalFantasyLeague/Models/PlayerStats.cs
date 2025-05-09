611ec39df349024056bec12486a0b6aa83287c39
Jake Heaney <jake.heaney@bnymellon.com>
2025-05-09 11:48:43 +0100

    minor styling nav menu tweaks

---
+++ LocalFantasyLeague/Models/PlayerStats.cs
@@ -0,0 +1,15 @@
+﻿namespace LocalFantasyLeague.Models;
+
+public class PlayerStats
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Appearances { get; set; }
+    public int Goals { get; set; }
+    public int Assists { get; set; }
+    public int YellowCards { get; set; }
+    public int RedCards { get; set; }
+    public int PenaltySaves { get; set; }
+    public int CleanSheets { get; set; }
+    public int PenaltyMissed { get; set; }
+}
---
+++ LocalFantasyLeague/Models/SeasonResult.cs
@@ -0,0 +1,13 @@
+﻿namespace LocalFantasyLeague.Models;
+
+public class SeasonResult
+{
+    public int SeasonId { get; set; }
+    public string SeasonName { get; set; } = string.Empty;
+    public DateTime SeasonStartDate { get; set; }
+    public DateTime SeasonEndDate { get; set; }
+    public int TotalMatches { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int Draws { get; set; }
+}
